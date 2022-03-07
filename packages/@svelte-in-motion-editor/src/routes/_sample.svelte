a63c439e0ffbfa8a41d1bc3eb2cca6764e3b0371
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-07 04:38:43 -0500

    - Updated sample to have a sibling background animation

--- packages/@svelte-in-motion-editor/src/routes/_sample.svelte
+++ packages/@svelte-in-motion-editor/src/routes/_sample.svelte
@@ -1,4 +1,32 @@
-<div style="width:max-content;height:max-content">
+<div class="content">
+    <Translate.In
+        class="background--transition"
+        delay={0.25}
+        duration={1.25}
+        easing={bounceOut}
+        units_y="-100px"
+    >
+        <Fade.In class="background--transition" delay={0.25} duration={1.25}>
+            <Fade.Out class="background--transition" delay={2} duration={0.5}>
+                <Translate.Out
+                    class="background--transition"
+                    delay={2}
+                    duration={0.5}
+                    units_y="-100px"
+                >
+                    <Rotate.Out
+                        class="background--transition"
+                        delay={2}
+                        duration={0.5}
+                        rotation="-90deg"
+                    >
+                        <div class="background--box" />
+                    </Rotate.Out>
+                </Translate.Out>
+            </Fade.Out>
+        </Fade.In>
+    </Translate.In>
+
     <Scale.In delay={0.25} duration={1.25} easing={bounceOut}>
         <Fade.Out delay={2} duration={0.5}>
             <Translate.Out delay={2} duration={0.5} units_y="100px">
@@ -9,3 +37,29 @@
         </Fade.Out>
     </Scale.In>
 </div>
+
+<style>
+    :global(.content) {
+        display: flex;
+        position: relative;
+
+        align-items: center;
+        justify-content: center;
+
+        width: 15rem;
+        height: 7.5rem;
+    }
+
+    :global(.background--transition) {
+        position: absolute;
+
+        inset: 0;
+    }
+
+    :global(.background--box) {
+        position: absolute;
+        inset: 0;
+
+        background: skyblue;
+    }
+</style>
