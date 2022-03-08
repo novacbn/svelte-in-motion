0d10f9d5d69c75f4ea2fd37957929426bc43597f
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-08 13:49:33 -0500

    - Updated rng stores to reflect recent store changes

--- packages/@svelte-in-motion-core/src/stores/random.ts
+++ packages/@svelte-in-motion-core/src/stores/random.ts
@@ -3,13 +3,17 @@ import {derived} from "svelte/store";
 import MersenneTwister from "mersenne-twister";
 import stringHash from "string-hash";
 
+import type {IFrameStore} from "./frame";
 import {CONTEXT_FRAME} from "./frame";
+import type {ReadableOnly} from "./util";
 
 const DEFAULT_SEED = "svelte-in-motion";
 
 export type IRandomStore = Readable<number>;
 
 export interface IRandomOptions {
+    frame: ReadableOnly<IFrameStore>;
+
     seed?: string;
 }
 
@@ -19,9 +23,59 @@ export interface IRandomRangeOptions extends IRandomOptions {
     start: number;
 }
 
-export function random(options: IRandomOptions = {}): IRandomStore {
-    const {seed = DEFAULT_SEED} = options;
-    const frame = CONTEXT_FRAME.get()!;
+export const CONTEXT_RANDOM = {
+    has() {
+        return CONTEXT_FRAME.has();
+    },
+
+    get(options: Omit<IRandomOptions, "frame">) {
+        const frame = CONTEXT_FRAME.get();
+        if (!frame) {
+            throw new ReferenceError(
+                `bad dispatch to 'CONTEXT_RANDOM.get' (context 'CONTEXT_FRAME' not available)`
+            );
+        }
+
+        return random({...options, frame});
+    },
+};
+
+export const CONTEXT_RANDOM_FLOAT = {
+    has() {
+        return CONTEXT_FRAME.has();
+    },
+
+    get(options: Omit<IRandomRangeOptions, "frame">) {
+        const frame = CONTEXT_FRAME.get();
+        if (!frame) {
+            throw new ReferenceError(
+                `bad dispatch to 'CONTEXT_RANDOM_FLOAT.get' (context 'CONTEXT_FRAME' not available)`
+            );
+        }
+
+        return random_float({...options, frame});
+    },
+};
+
+export const CONTEXT_RANDOM_INTEGER = {
+    has() {
+        return CONTEXT_FRAME.has();
+    },
+
+    get(options: Omit<IRandomRangeOptions, "frame">) {
+        const frame = CONTEXT_FRAME.get();
+        if (!frame) {
+            throw new ReferenceError(
+                `bad dispatch to 'CONTEXT_RANDOM_FLOAT.get' (context 'CONTEXT_FRAME' not available)`
+            );
+        }
+
+        return random_integer({...options, frame});
+    },
+};
+
+export function random(options: IRandomOptions): IRandomStore {
+    const {frame, seed = DEFAULT_SEED} = options;
 
     return derived(frame, ($frame) => {
         const hash = stringHash(`${seed}${$frame}`);
