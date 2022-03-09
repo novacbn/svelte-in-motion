cee3b46f7023f39d9461344c4abce654a666623c
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-09 08:32:23 -0500

    - Added dev server headers for WASM ffmpeg

--- packages/@svelte-in-motion-editor/vite.config.ts
+++ packages/@svelte-in-motion-editor/vite.config.ts
@@ -15,5 +15,17 @@ export default defineConfig({
         },
     },
 
-    plugins: [svelte()],
+    plugins: [
+        svelte(),
+        {
+            name: "configure-response-headers",
+            configureServer: (server) => {
+                server.middlewares.use((_req, res, next) => {
+                    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+                    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+                    next();
+                });
+            },
+        },
+    ],
 });
