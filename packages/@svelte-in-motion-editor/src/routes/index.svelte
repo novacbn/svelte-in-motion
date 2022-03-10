b2419cbd8a3b303640d50e736a82950f84ff0e8c
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-10 08:03:34 -0500

    - Redirect dummy homepage to editor sample

--- packages/@svelte-in-motion-editor/src/routes/index.svelte
+++ packages/@svelte-in-motion-editor/src/routes/index.svelte
@@ -1,1 +1,13 @@
+<script context="module" lang="ts">
+    import type {ILoadCallback} from "../lib/router";
+
+    export const pattern: string = "/";
+
+    export const load: ILoadCallback = () => {
+        return {
+            redirect: "/editor/Sample.svelte",
+        };
+    };
+</script>
+
 hello world!
