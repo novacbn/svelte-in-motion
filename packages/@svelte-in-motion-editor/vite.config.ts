500ba888810e3ea78f24d412965a57a3603e1856
novacbn <31122674+novacbn@users.noreply.github.com>
2022-03-26 17:12:23 -0400

    - Updated vite config to use new built-in configuration for dev server headers

--- packages/@svelte-in-motion-editor/vite.config.ts
+++ packages/@svelte-in-motion-editor/vite.config.ts
@@ -17,17 +17,12 @@ export default defineConfig({
         },
     },
 
-    plugins: [
-        svelte(),
-        {
-            name: "configure-response-headers",
-            configureServer: (server) => {
-                server.middlewares.use((_req, res, next) => {
-                    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-                    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-                    next();
-                });
-            },
+    server: {
+        headers: {
+            "Cross-Origin-Embedder-Policy": "require-corp",
+            "Cross-Origin-Opener-Policy": "same-origin",
         },
-    ],
+    },
+
+    plugins: [svelte()],
 });
