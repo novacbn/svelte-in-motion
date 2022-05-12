58ae299faebb73d898058204cf071f296923daa4
novacbn <31122674+novacbn@users.noreply.github.com>
2022-05-12 18:05:59 -0400

    - Moved event helpers to their own directory - Updated `channel` to accept external `BroadcastChannel` instances - Removed `channel` from replicating messages to current process - Removed `channel` closing the channel when no subscribers exists - Added `channel.close` which proxies to the internal `BroadcastChannel.close`

---
+++ packages/@svelte-in-motion-utilities/src/events/channel.ts
@@ -0,0 +1,44 @@
+import type {IEvent, IEventNotifier} from "./event";
+import {event} from "./event";
+
+export interface IChannelEvent<T> extends IEvent<T> {
+    close(): void;
+}
+
+export function channel<T>(
+    channel: BroadcastChannel | string,
+    start?: IEventNotifier<T>
+): IChannelEvent<T> {
+    channel = typeof channel === "string" ? new BroadcastChannel(channel) : channel;
+
+    const {subscribe} = event<T>((dispatch) => {
+        function on_message(event: MessageEvent) {
+            dispatch(event.data);
+        }
+
+        (channel as BroadcastChannel).addEventListener("message", on_message);
+
+        let destroy = start ? start(dispatch) : null;
+
+        return () => {
+            (channel as BroadcastChannel).removeEventListener("message", on_message);
+
+            if (destroy) {
+                destroy();
+                destroy = null;
+            }
+        };
+    });
+
+    return {
+        close() {
+            (channel as BroadcastChannel).close();
+        },
+
+        dispatch(detail) {
+            (channel as BroadcastChannel).postMessage(detail);
+        },
+
+        subscribe,
+    };
+}
--- packages/@svelte-in-motion-utilities/src/index.ts
+++ packages/@svelte-in-motion-utilities/src/index.ts
@@ -1,13 +1,14 @@
+export * from "./events/channel";
+export * from "./events/event";
+
 export * from "./stores/collection";
 export * from "./stores/immutable";
 export * from "./stores/map";
 export * from "./stores/types";
 
-export * from "./util/channel";
 export * from "./util/contexts";
 export * from "./util/eval";
 export * from "./util/environment";
-export * from "./util/event";
 export * from "./util/math";
 export * from "./util/random";
 export * from "./util/string";
