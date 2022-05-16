f79b8067a85d381dd848457999171c25739dfc9f
novacbn <31122674+novacbn@users.noreply.github.com>
2022-05-15 23:07:07 -0400

    - Removed error store from per-Editor to per-Workspace

--- packages/@svelte-in-motion-editor/src/lib/editor.ts
+++ packages/@svelte-in-motion-editor/src/lib/editor.ts
@@ -3,17 +3,11 @@ import {get} from "svelte/store";
 import type {IFrameStore, IPlayingStore} from "@svelte-in-motion/core";
 import {frame as frame_store, playing as playing_store} from "@svelte-in-motion/core";
 import {make_scoped_context} from "@svelte-in-motion/utilities";
-
-import {prompts} from "./stores/prompts";
-import type {IErrorsStore} from "./stores/errors";
-import {errors as make_errors_store} from "./stores/errors";
 import {IDriver, IFileTextStore, preload_text} from "@svelte-in-motion/storage";
 
 export const CONTEXT_EDITOR = make_scoped_context<IEditorContext>("editor");
 
 export interface IEditorContext {
-    errors: IErrorsStore;
-
     frame: IFrameStore;
 
     file_path: string;
@@ -26,13 +20,10 @@ export interface IEditorContext {
 export async function editor(driver: IDriver, file_path: string): Promise<IEditorContext> {
     const text = await preload_text(driver, file_path);
 
-    const errors = make_errors_store();
-
     const frame = frame_store(0);
     const playing = playing_store(false);
 
     return {
-        errors,
         frame,
         file_path,
         playing,
