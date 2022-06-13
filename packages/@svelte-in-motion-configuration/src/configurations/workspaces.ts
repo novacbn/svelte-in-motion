29d7dc229af218971d3c45142c154d678f07b326
novacbn <31122674+novacbn@users.noreply.github.com>
2022-06-13 16:26:01 -0400

    - Colocated driver creation into `@svelte-in-motion/configuration` subpackage

--- packages/@svelte-in-motion-configuration/src/configurations/workspaces.ts
+++ packages/@svelte-in-motion-configuration/src/configurations/workspaces.ts
@@ -1,6 +1,18 @@
-import {Alphanumeric, DataClass, UUID, uuid} from "@svelte-in-motion/type";
+import type {IDriver} from "@svelte-in-motion/storage";
+import {indexeddb} from "@svelte-in-motion/storage";
+import {Alphanumeric, UUID, DataClass, uuid} from "@svelte-in-motion/type";
 
 import {Configuration} from "./configuration";
+
+const STORAGE_DRIVERS = {
+    indexeddb: {
+        async make(metadata: WorkspacesItemConfiguration): Promise<IDriver> {
+            const {identifier} = metadata;
+
+            return indexeddb(`sim:workspace:${identifier}`);
+        },
+    },
+};
 
 export class WorkspacesItemStorageConfiguration extends DataClass {
     // TODO: This needs to be dynamic some how... Otherwise extensions wouldn't be able to add more drivers
@@ -19,6 +31,20 @@ export class WorkspacesItemConfiguration extends DataClass {
     identifier: UUID = uuid();
 
     storage: WorkspacesItemStorageConfiguration = new WorkspacesItemStorageConfiguration();
+
+    make_driver(): Promise<IDriver> {
+        // TODO: this needs to become extendable via extensions
+        const {driver: driver_name} = this.storage;
+        const driver = STORAGE_DRIVERS[driver_name];
+
+        if (!driver) {
+            throw new ReferenceError(
+                `bad argument #0 to 'make_driver' (invalid driver name '${driver_name}')`
+            );
+        }
+
+        return driver.make(this);
+    }
 }
 
 export class WorkspacesConfiguration extends Configuration {
