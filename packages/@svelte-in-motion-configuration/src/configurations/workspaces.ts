928965d687834fb332cefbed3f59b2ea6e27040b
novacbn <31122674+novacbn@users.noreply.github.com>
2022-06-12 21:44:12 -0400

    - Separated `WorkspacesItemConfiguration.storage` typing into a `DataClass`

--- packages/@svelte-in-motion-configuration/src/configurations/workspaces.ts
+++ packages/@svelte-in-motion-configuration/src/configurations/workspaces.ts
@@ -2,6 +2,13 @@ import {Alphanumeric, DataClass, UUID, uuid} from "@svelte-in-motion/type";
 
 import {Configuration} from "./configuration";
 
+export class WorkspacesItemStorageConfiguration extends DataClass {
+    // TODO: This needs to be dynamic some how... Otherwise extensions wouldn't be able to add more drivers
+    readonly driver: "indexeddb" = "indexeddb";
+
+    readonly configuration?: Record<string, boolean | number | null | string>;
+}
+
 export class WorkspacesItemConfiguration extends DataClass {
     readonly name!: string & Alphanumeric;
 
@@ -9,14 +16,7 @@ export class WorkspacesItemConfiguration extends DataClass {
 
     readonly last_accessed: Date = new Date();
 
-    readonly storage: {
-        // TODO: This needs to be dynamic some how... Otherwise extensions wouldn't be able to add more drivers
-        readonly driver: "indexeddb";
-
-        readonly configuration?: Record<string, boolean | number | null | string>;
-    } = {
-        driver: "indexeddb",
-    };
+    readonly storage: WorkspacesItemStorageConfiguration = new WorkspacesItemStorageConfiguration();
 }
 
 export class WorkspacesConfiguration extends Configuration {
