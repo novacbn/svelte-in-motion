9a6ea7ce083eb1257f20ec84fbda547c4aad513f
novacbn <31122674+novacbn@users.noreply.github.com>
2022-06-13 21:19:53 -0400

    - Added sketch for encoding RPC

--- packages/@svelte-in-motion-encoding/src/rpc/agent.ts
+++ packages/@svelte-in-motion-encoding/src/rpc/agent.ts
@@ -1,7 +1,39 @@
 import {ControllerSymbol} from "@svelte-in-motion/rpc";
+import {UUID} from "@svelte-in-motion/type";
+
+import type {ICodecNames} from "../encoding/codec";
+import type {IEncodingOptions} from "../encoding/encode";
 
 export const IRPCEncodingAgentController =
     ControllerSymbol<IRPCEncodingAgentController>("Encoding.Agent");
 export interface IRPCEncodingAgentController {
-    get_version(): Promise<string>;
+    cancel_job(identifier: UUID): Promise<void>;
+
+    get_available_codecs(): Promise<ICodecNames[]>;
+
+    get_available_crf_range(codec: ICodecNames): Promise<[number, number]>;
+
+    get_available_dimensions_ranges(
+        codec: ICodecNames
+    ): Promise<[[number, number], [number, number]]>;
+
+    get_available_framerate_range(
+        codec: ICodecNames,
+        width: number,
+        height: number
+    ): Promise<[number, number]>;
+
+    get_default_codec(): Promise<ICodecNames>;
+
+    get_default_crf(codec: ICodecNames): Promise<number>;
+
+    get_default_dimensions(codec: ICodecNames): Promise<number>;
+
+    get_default_framerate(codec: ICodecNames, width: number, height: number): Promise<number>;
+
+    get_agent_version(): Promise<[number, number, number]>;
+
+    get_protocol_version(): Promise<[number, number, number]>;
+
+    start_job(options: IEncodingOptions): Promise<UUID>;
 }
--- packages/@svelte-in-motion-encoding/src/rpc/host.ts
+++ packages/@svelte-in-motion-encoding/src/rpc/host.ts
@@ -1,7 +1,16 @@
 import {ControllerSymbol} from "@svelte-in-motion/rpc";
+import {UUID} from "@svelte-in-motion/type";
 
 export const IRPCEncodingHostController =
     ControllerSymbol<IRPCEncodingHostController>("Encoding.Host");
 export interface IRPCEncodingHostController {
-    get_version(): Promise<string>;
+    JOB_END(identifier: UUID, video: Uint8Array): Promise<void>;
+
+    JOB_PROGRESS(identifier: UUID, completion: number): Promise<void>;
+
+    JOB_START(identifier: UUID): Promise<void>;
+
+    get_host_version(): Promise<[number, number, number]>;
+
+    get_protocol_version(): Promise<[number, number, number]>;
 }
