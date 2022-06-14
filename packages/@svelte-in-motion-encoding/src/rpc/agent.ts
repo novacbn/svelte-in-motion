07a798aa30426e0e574d25bf3e15ed4a1661debd
novacbn <31122674+novacbn@users.noreply.github.com>
2022-06-13 20:44:44 -0400

    - Removed RPC implementation class from `@svelte-in-motion/encoding`
        - Ultimately each agent (encoding, rendering, etc) might have diverging implementations. e.g. a remote rendering agent might look different than a `WebWorker` spawned by the app

--- packages/@svelte-in-motion-encoding/src/rpc/agent.ts
+++ packages/@svelte-in-motion-encoding/src/rpc/agent.ts
@@ -1,14 +1,7 @@
-import {ControllerSymbol, rpc} from "@svelte-in-motion/rpc";
+import {ControllerSymbol} from "@svelte-in-motion/rpc";
 
 export const IRPCEncodingAgentController =
     ControllerSymbol<IRPCEncodingAgentController>("Encoding.Agent");
 export interface IRPCEncodingAgentController {
     get_version(): Promise<string>;
-}
-
-@rpc.controller(IRPCEncodingAgentController)
-export class RPCEncodingAgentController implements IRPCEncodingAgentController {
-    async get_version(): Promise<string> {
-        return "0.0.0";
-    }
 }
--- packages/@svelte-in-motion-encoding/src/rpc/host.ts
+++ packages/@svelte-in-motion-encoding/src/rpc/host.ts
@@ -1,14 +1,7 @@
-import {ControllerSymbol, rpc} from "@svelte-in-motion/rpc";
+import {ControllerSymbol} from "@svelte-in-motion/rpc";
 
 export const IRPCEncodingHostController =
     ControllerSymbol<IRPCEncodingHostController>("Encoding.Host");
 export interface IRPCEncodingHostController {
     get_version(): Promise<string>;
-}
-
-@rpc.controller(IRPCEncodingHostController)
-export class RPCEncodingHostController implements IRPCEncodingHostController {
-    async get_version(): Promise<string> {
-        return "0.0.0";
-    }
 }
