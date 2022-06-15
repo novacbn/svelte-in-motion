78a87aaf6cd91fe341e6097f152e2dae8237ce85
novacbn <31122674+novacbn@users.noreply.github.com>
2022-06-15 19:46:31 -0400

    - Fixed keybind handler skipping any bind past the first one - Pre-lowercased bind keys on push

--- packages/@svelte-in-motion-editor/src/lib/stores/keybinds.ts
+++ packages/@svelte-in-motion-editor/src/lib/stores/keybinds.ts
@@ -50,69 +50,64 @@ export function keybinds(app: IAppContext): IKeybindsStore {
             for (const item of items) {
                 const {binds, identifier, repeat: can_repeat = false, repeat_throttle = 0} = item;
 
-                let can_handle = false;
-                for (const bind of binds) {
-                    if (can_handle) break;
+                const can_handle = !!binds.find((bind) => bind.includes(key));
+                if (can_handle) {
+                    let current_active = false;
+                    for (const bind of binds) {
+                        if (current_active) break;
+                        current_active = true;
 
-                    for (const bind_key of bind) {
-                        if (bind_key.toLowerCase() === key) {
-                            can_handle = true;
+                        for (const bind_key of bind) {
+                            if (!key_lookup.has(bind_key)) {
+                                current_active = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    const previous_active = bind_lookup.has(identifier);
+                    if (current_active) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    } else if (can_repeat) timestamp_lookup.delete(identifier);
+
+                    const current_timestamp = Date.now();
+                    const is_repeating = repeat && can_repeat;
+
+                    if (is_repeating) {
+                        const previous_timestamp = timestamp_lookup.get(identifier) ?? 0;
+                        if (current_timestamp - previous_timestamp < repeat_throttle) {
                             break;
                         }
                     }
-                }
 
-                if (!can_handle) break;
+                    if (current_active !== previous_active || is_repeating) {
+                        item.on_bind(app, {
+                            active: current_active,
+                            repeat,
+                        });
 
-                let current_active = false;
-                for (const bind of binds) {
-                    if (current_active) break;
-                    current_active = true;
-
-                    for (const bind_key of bind) {
-                        if (!key_lookup.has(bind_key.toLowerCase())) {
-                            current_active = false;
-                            break;
-                        }
+                        if (is_repeating) timestamp_lookup.set(identifier, current_timestamp);
                     }
+
+                    if (current_active) bind_lookup.set(identifier, true);
+                    else bind_lookup.delete(identifier);
+
+                    break;
                 }
-
-                const previous_active = bind_lookup.has(identifier);
-                if (current_active) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                } else if (can_repeat) timestamp_lookup.delete(identifier);
-
-                const current_timestamp = Date.now();
-                const is_repeating = repeat && can_repeat;
-
-                if (is_repeating) {
-                    const previous_timestamp = timestamp_lookup.get(identifier) ?? 0;
-                    if (current_timestamp - previous_timestamp < repeat_throttle) {
-                        break;
-                    }
-                }
-
-                if (current_active !== previous_active || is_repeating) {
-                    item.on_bind(app, {
-                        active: current_active,
-                        repeat,
-                    });
-
-                    if (is_repeating) timestamp_lookup.set(identifier, current_timestamp);
-                }
-
-                if (current_active) bind_lookup.set(identifier, true);
-                else bind_lookup.delete(identifier);
-
-                break;
             }
         },
 
         find,
         has,
 
-        push,
+        push(item) {
+            return push({
+                ...item,
+                binds: item.binds.map((bind) => bind.map((key) => key.toLowerCase())),
+            });
+        },
+
         subscribe,
 
         remove,
