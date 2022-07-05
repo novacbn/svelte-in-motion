086648c7fa70edec4bdfac5e5b0171a533293900
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-05 02:34:36 -0400

    - Added dash case formatting utility

--- packages/@svelte-in-motion-utilities/src/util/string.ts
+++ packages/@svelte-in-motion-utilities/src/util/string.ts
@@ -5,6 +5,13 @@ export function format_camel_case(text: string): string {
     return text.replace(/-(\w)/g, (match, character) => character.toUpperCase());
 }
 
+export function format_dash_case(text: string): string {
+    return text
+        .replace(/([A-Z])/g, (match, character) => "-" + character.toLowerCase())
+        .replace(/([-]+)/g, "-")
+        .replace(/(^-)/, "");
+}
+
 export function format_slug(text: string): string {
     // NOTE: Proxying incase implementation changes are wanted later
 
