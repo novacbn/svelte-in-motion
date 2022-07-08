af358f822e4d6b55da5f3c5f2671aab76dff8480
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-08 06:46:59 -0400

    - Added set height to status bar

--- packages/@svelte-in-motion-editor/src/components/app/AppStatus.svelte
+++ packages/@svelte-in-motion-editor/src/components/app/AppStatus.svelte
@@ -2,7 +2,7 @@
     import {Box, Stack} from "@kahi-ui/framework";
 </script>
 
-<Box class="sim--app-status" palette="accent" padding_x="tiny" padding_y="nano">
+<Box class="sim--app-status" palette="accent" padding_x="tiny" padding_y="nano" height="icon-tiny">
     <Stack.Container orientation="horizontal" spacing="small" alignment_y="center" height="100">
         <slot />
     </Stack.Container>
