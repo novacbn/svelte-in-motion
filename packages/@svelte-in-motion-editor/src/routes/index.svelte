661aa28edb9c8358f0ae18ac3cb48257751a341f
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-08 06:14:58 -0400

    - Redesigned dashboard

--- packages/@svelte-in-motion-editor/src/routes/index.svelte
+++ packages/@svelte-in-motion-editor/src/routes/index.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <script lang="ts">
-    import {Button, Hero, Stack, Tile, Text} from "@kahi-ui/framework";
+    import {Anchor, Badge, Heading, Spacer, Stack, Text} from "@kahi-ui/framework";
     //import {PackageX} from "lucide-svelte";
 
     import AppLayout from "../components/app/AppLayout.svelte";
@@ -21,75 +21,106 @@
 
     const {commands, translations, workspaces} = CONTEXT_APP.get()!;
 
-    async function on_new_click(event: MouseEvent): Promise<void> {
+    function on_new_click(event: MouseEvent): void {
+        event.preventDefault();
+
         commands.execute("workspace.prompt.new");
     }
 
-    $: recent_workspaces = $workspaces.workspaces.slice().sort((workspace_a, workspace_b) => {
-        const {seconds} = workspace_a.accessed_at.until(workspace_b.accessed_at, {
-            largestUnit: "seconds",
-            smallestUnit: "seconds",
-        });
+    function on_recent_click(event: MouseEvent): void {
+        event.preventDefault();
 
-        return seconds > 0 ? 1 : seconds < 0 ? -1 : 0;
-    });
+        commands.execute("workspace.prompt.open_recent");
+    }
+
+    function on_template_click(event: MouseEvent): void {
+        event.preventDefault();
+
+        commands.execute("workspace.prompt.new_from_template");
+    }
+
+    $: recent_workspaces = $workspaces.workspaces
+        .slice()
+        .sort((workspace_a, workspace_b) => {
+            const {seconds} = workspace_a.accessed_at.until(workspace_b.accessed_at, {
+                largestUnit: "seconds",
+                smallestUnit: "seconds",
+            });
+
+            return seconds < 0 ? -1 : 0;
+        })
+        .slice(0, 5);
 </script>
 
 <AppLayout>
-    {#if recent_workspaces.length > 0}
-        <Stack.Container spacing="small" padding="small">
-            {#each recent_workspaces as workspace (workspace.identifier)}
-                <Tile.Container sizing="nano" height="content-max">
-                    <Tile.Section>
-                        <Tile.Header>{workspace.name}</Tile.Header>
-                        <Text is="small">
-                            {$translations.format(
-                                "ui-view-dashboard-last_accessed_workspace-label",
-                                {
-                                    timestamp: workspace.format_accessed(),
-                                }
-                            )}
-                        </Text>
-                    </Tile.Section>
+    <Stack.Container
+        class="sim--app-dashboard"
+        spacing="large"
+        alignment_x="left"
+        alignment_y="center"
+        padding_x="massive"
+    >
+        <Heading>Svelte-In-Motion</Heading>
 
-                    <Tile.Footer>
-                        <Button
-                            is="a"
-                            href="#/workspace/{workspace.identifier}"
-                            variation="clear"
-                            palette="accent"
-                            sizing="nano"
+        <div>
+            <Heading is="h2" margin_bottom="medium">Start</Heading>
+
+            <Stack.Container alignment_x="left">
+                <Anchor href="#" palette="accent" on:click={on_new_click}>New Workspace</Anchor>
+
+                <Anchor href="#" palette="accent" on:click={on_template_click}>
+                    New Workspace from Template
+                </Anchor>
+
+                <Anchor href="#" palette="accent" on:click={on_recent_click}>
+                    Open Recent Workspace
+                </Anchor>
+            </Stack.Container>
+        </div>
+
+        {#if recent_workspaces.length > 0}
+            <div>
+                <Heading is="h2" margin_bottom="medium">Recent</Heading>
+
+                <Stack.Container max_width="content-max">
+                    {#each recent_workspaces as workspace (workspace.identifier)}
+                        <Stack.Container
+                            orientation="horizontal"
+                            alignment_y="center"
+                            spacing="medium"
                         >
-                            {$translations.format("ui-view-dashboard-open_workspace-label")}
-                        </Button>
-                    </Tile.Footer>
-                </Tile.Container>
-            {/each}
-        </Stack.Container>
+                            <Anchor href="#/workspace/{workspace.identifier}" palette="accent">
+                                {workspace.name}
+                            </Anchor>
 
-        <Button palette="affirmative" on:click={on_new_click}>
-            {$translations.format("ui-view-dashboard-new_workspace-label")}
-        </Button>
-    {:else}
-        <Hero.Container class="sim--app-dashboard">
-            <Hero.Header>
-                <!--
-                    <PackageX size="1em" strokeWidth="1" />
-                -->
-                NOT FOUND
-            </Hero.Header>
+                            <Spacer />
 
-            <Hero.Section>
-                {$translations.format("ui-view-dashboard-no_available_workspaces-label")}
-            </Hero.Section>
+                            <Text sizing="nano">
+                                <Badge radius="tiny">
+                                    {workspace.format_accessed()}
+                                </Badge>
+                            </Text>
+                        </Stack.Container>
+                    {/each}
 
-            <Hero.Footer>
-                <Button palette="affirmative" on:click={on_new_click}>
-                    {$translations.format("ui-view-dashboard-new_workspace-label")}
-                </Button>
-            </Hero.Footer>
-        </Hero.Container>
-    {/if}
+                    {#if $workspaces.workspaces.length > recent_workspaces.length}
+                        <Anchor
+                            href="#"
+                            palette="accent"
+                            max_width="content-max"
+                            on:click={on_recent_click}
+                        >
+                            ...More
+                        </Anchor>
+                    {/if}
+                </Stack.Container>
+            </div>
+        {/if}
+    </Stack.Container>
+
+    <!--
+        <AppStatus />
+    -->
 </AppLayout>
 
 <style>
