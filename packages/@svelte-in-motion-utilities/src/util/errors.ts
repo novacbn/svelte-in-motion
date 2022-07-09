15adcfd23648213d824be8f6653c892ac73f269b
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-09 13:46:24 -0400

    - Added `UserError` for descrbing errors that should be surfaced to the end user

--- packages/@svelte-in-motion-utilities/src/util/errors.ts
+++ packages/@svelte-in-motion-utilities/src/util/errors.ts
@@ -1,5 +1,29 @@
-export class PromptDismissError extends Error {
+import type {SvelteComponent} from "svelte";
+
+export class PromptError extends Error {
+    name = PromptError.name;
+}
+
+export class PromptDismissError extends PromptError {
     message = "Prompt was dismissed";
 
     name = PromptDismissError.name;
+}
+
+export class UserError<T = void> extends Error {
+    // TODO: Once icon situation is resolved, should default to the X icon
+    icon?: typeof SvelteComponent;
+
+    name = UserError.name;
+
+    tokens?: T;
+
+    constructor(tokens?: T, icon?: typeof SvelteComponent) {
+        // @ts-expect-error - HACK: TypeScript just doesn't like that kind
+        // of thing, but we can access it
+        super(this.name);
+
+        this.icon = icon;
+        this.tokens = tokens;
+    }
 }
