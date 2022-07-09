d7710abc0c87dcc5d7a57abbc250525b84276336
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-09 08:06:10 -0400

    - Added ignores since Editor-dependent subpackages aren't aware of Vite or the JSON importing

--- packages/@svelte-in-motion-editor/src/lib/util/constants.ts
+++ packages/@svelte-in-motion-editor/src/lib/util/constants.ts
@@ -1,7 +1,9 @@
+// @ts-ignore
 import PACKAGE from "../../../package.json";
 
 import {append_pathname} from "@svelte-in-motion/utilities";
 
 export const APPLICATION_VERSION = PACKAGE.version;
 
+// @ts-ignore
 export const APPLICATION_URL = append_pathname(location.origin, import.meta.env.BASE_URL);
