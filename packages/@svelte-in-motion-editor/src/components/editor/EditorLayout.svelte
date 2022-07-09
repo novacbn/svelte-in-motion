93824f4172fa2875f1c9d3e1b08540eba7d0d3dc
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-09 07:26:12 -0400

    - Updated editor layout to hide third column when preview is UI is invisible

--- packages/@svelte-in-motion-editor/src/components/editor/EditorLayout.svelte
+++ packages/@svelte-in-motion-editor/src/components/editor/EditorLayout.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
     import AppHeader from "../app/AppHeader.svelte";
     import AppNotifications from "../app/AppNotifications.svelte";
     import AppPrompts from "../app/AppPrompts.svelte";
+
+    import {CONTEXT_APP} from "../../lib/app";
+    import {CONTEXT_PREVIEW} from "../../lib/preview";
+
+    const {preferences} = CONTEXT_APP.get()!;
+    const preview = CONTEXT_PREVIEW.get();
+
+    $: has_preview =
+        preview && ($preferences.ui.preview.controls || $preferences.ui.preview.viewport);
 </script>
 
-<div class="sim--editor-layout">
+<div class="sim--editor-layout" data-has-preview={has_preview ? true : undefined}>
     <slot />
 
     <!-- NOTE: This order is specific so they layer properly -->
@@ -29,17 +38,28 @@
     :global(.sim--editor-layout) {
         display: grid;
 
+        grid-template-columns: max-content 1fr max-content;
+        grid-template-rows: max-content 1fr max-content max-content max-content;
+
+        width: min(100vw, 100%);
+        height: min(100vh, 100%);
+    }
+
+    :global(.sim--editor-layout[data-has-preview]) {
         grid-template-areas:
             "header header header"
             "sidebar script viewport"
             "sidebar script controls"
             "timeline timeline timeline"
             "status status status";
+    }
 
-        grid-template-columns: max-content max-content 1fr;
-        grid-template-rows: max-content 1fr max-content max-content max-content;
-
-        width: min(100vw, 100%);
-        height: min(100vh, 100%);
+    :global(.sim--editor-layout:not([data-has-preview])) {
+        grid-template-areas:
+            "header header"
+            "sidebar script"
+            "sidebar script"
+            "timeline timeline"
+            "status status";
     }
 </style>
