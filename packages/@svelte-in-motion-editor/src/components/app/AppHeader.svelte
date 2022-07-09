8665ecfea06ccc0b285322297c99d8eaedc0ce28
novacbn <31122674+novacbn@users.noreply.github.com>
2022-07-09 04:45:33 -0400

    - Thinking it and inspired by VSCode's new command center. The toolbar is being removed in favor of the command palette only.

--- packages/@svelte-in-motion-editor/src/components/app/AppHeader.svelte
+++ packages/@svelte-in-motion-editor/src/components/app/AppHeader.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-    import {Box, Divider, Stack, Text} from "@kahi-ui/framework";
+    import {Anchor, Box, Divider, Stack, Text} from "@kahi-ui/framework";
 
-    import AppToolbar from "./AppToolbar.svelte";
+    import {CONTEXT_APP} from "../../lib/app";
+
+    const {commands} = CONTEXT_APP.get()!;
+
+    function on_palette_click(event: MouseEvent): void {
+        event.preventDefault();
+
+        commands.execute("palette.prompt.commands");
+    }
 </script>
 
 <Box class="sim--app-header" palette="auto">
@@ -12,9 +20,9 @@
         padding_x="medium"
         padding_top="tiny"
     >
-        <Text is="strong" sizing="tiny">Svelte-In-Motion</Text>
-
-        <AppToolbar />
+        <Text is="strong" sizing="tiny">
+            <Anchor href="#" on:click={on_palette_click}>Svelte-In-Motion</Anchor>
+        </Text>
     </Stack.Container>
 
     <Divider palette="inverse" margin="none" margin_top="tiny" />
